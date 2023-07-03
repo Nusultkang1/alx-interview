8d17af077d64dab9ea44fd59cb25b874a9a51ed4
nurusul <nurudeensultan28@gmail.com>
2023-07-03 04:55:56 +0100

    adjustments made

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,6 +1,17 @@
 #!/usr/bin/python3
+"""
+Creates a function def pascal_triangle(n)
+Returns a list of lists of integers
+represents the pascal's triangle of n rows
+Returns and empty list if n <= 0
+"""
+
 def print_pascal_triangle(n):
-    # Create an empty list to store the triangle
+    """
+    function that prints pascal's triangle of
+    n rows if n > 0, and returns an empty list if n <= 0
+    """
+
     triangle = []
 
     for i in range(n):
