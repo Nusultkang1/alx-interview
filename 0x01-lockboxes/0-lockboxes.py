26872941e134c3743ee1bd2e393e99fd39f045ce
nurusul <nurudeensultan28@gmail.com>
2023-07-09 14:45:14 +0100

    completed task

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""function that checks all boxes (functions) and returns True if they can be opened and False if they can't"""
+
+def canUnlockAll(boxes):
+    """defines a function to examnine boxes and determine their openability"""
+
+    n = len(boxes)
+    unlocked = [False] * n  # Track the unlocked status of each box
+    unlocked[0] = True  # The first box is initially unlocked
+    stack = [0]  # Start with the first box in the stack
+
+    while stack:
+        box = stack.pop()
+        keys = boxes[box]
+        for key in keys:
+            if key < n and not unlocked[key]:
+                unlocked[key] = True
+                stack.append(key)
+
+    return all(unlocked)
